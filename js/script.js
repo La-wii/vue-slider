674f53643e2744b0a6ae7c9b76da43cfbbfb593f
La-wii <sentilavale@gmail.com>
2021-06-22 18:15:38 +0200

    sistemata identazione

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const app = new Vue (
-    {
-        el: '#app',
-        data: {
-            counter: 0,
-            photos: [
-                'img/1.jpg',
-                'img/2.jpg',
-                'img/3.jpg',
-                'img/4.jpg'
-            ]
-        },
-        methods: {
-            nextPhoto(){
-                (this.counter == this.photos.length - 1) ? this.counter = 0 : this.counter++;
-            },
-            prevPhoto(){
-                (this.counter == 0) ? this.counter = photos.length - 1 : this.counter--;    
-            },
-            changePhoto(index){
-                this.counter = index;
-            }
-        }
-    }
+const app = new Vue ({
 
-);
+    el: '#app',
+    data: {
+        counter: 0,
+        photos: [
+            'img/1.jpg',
+            'img/2.jpg',
+            'img/3.jpg',
+            'img/4.jpg'     
+        ]
+    },
+
+    methods: {
+        nextPhoto(){
+            (this.counter == this.photos.length - 1) ? this.counter = 0 : this.counter++;
+        },
+        prevPhoto(){
+            (this.counter == 0) ? this.counter = photos.length - 1 : this.counter--;    
+        },
+        changePhoto(index){
+            this.counter = index;
+        }
+            
+    },
+    
+});
+
