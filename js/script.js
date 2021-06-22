f8e37ab689b93a550730c9dfba51fe5d41317d1b
La-wii <sentilavale@gmail.com>
2021-06-22 16:31:55 +0200

    nextPhoto() + prevPhoto()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 const app = new Vue (
     {
-        el: '#app'
+        el: '#app',
+        data: {
+            counter: 0,
+            photos: [
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg'
+            ]
+        },
+        methods: {
+            nextPhoto(){
+                (this.counter == this.photos.length - 1) ? this.counter = 0 : this.counter++;
+            },
+            prevPhoto(){
+                (this.counter == 0) ? this.counter = photos.length - 1 : this.counter--;    
+            }
+        }
     }
 
 );
